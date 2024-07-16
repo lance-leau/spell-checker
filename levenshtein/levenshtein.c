e054a7daf7e3263ec3f5ee42e6e5fecfa412ca22
HerosFuncky <kimtayantserrat@gmail.com>
2024-07-16 11:43:36 +0200

    changed for the swaps

--- levenshtein/levenshtein.c
+++ levenshtein/levenshtein.c
@@ -1,57 +1,70 @@
 #include "levenshtein.h"
 
-int min(int a, int b)
-{
-    return a > b ? b : a;
+// Function to find the minimum of three numbers
+int min3(int a, int b, int c) {
+    if (a < b && a < c) return a;
+    else if (b < c) return b;
+    else return c;
 }
 
-int distance(char* a, char* b)
-{
-    int n = strlen(a);
-    int m = strlen(b);
-   
-    int** d = malloc((n + 1) * sizeof(int*));
-    for (int i = 0; i <= n; i++) {
-        d[i] = malloc((m + 1) * sizeof(int));
+// Function to find the minimum of two numbers
+int min2(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+// Function to calculate the optimal string alignment (Levenshtein) distance with transpositions
+int distance(const char* s1, const char* s2) {
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+    int i, j;
+
+    // Create a table to store the results of subproblems
+    int** dp = (int**)malloc((len1 + 1) * sizeof(int*));
+    for (i = 0; i <= len1; i++) {
+        dp[i] = (int*)malloc((len2 + 1) * sizeof(int));
     }
-    int i,j = 0;
-    
-    for(i = 0;i <= n; i++)
-    {
-        d[i][0] = i;
+
+    // Initialize the table
+    for (i = 0; i <= len1; i++) {
+        dp[i][0] = i;
     }
-    
-    for(j = 0; j <= m; j++)
-    {
-        d[0][j] = j;
+    for (j = 0; j <= len2; j++) {
+        dp[0][j] = j;
     }
-    
-    for(i = 1; i <= n; i++)
-    {
-        for(j = 1; j <= m; j++)
-        {
-            if (a[i-1] == b[j-1]) {
-                d[i][j] = d[i-1][j-1];
+
+    // Populate the table using dynamic programming
+    for (i = 1; i <= len1; i++) {
+        for (j = 1; j <= len2; j++) {
+            if (s1[i-1] == s2[j-1]) {
+                dp[i][j] = dp[i-1][j-1];
             } else {
-                d[i][j] = 1 + min(d[i-1][j], min(d[i][j-1], d[i-1][j-1]));
+                int insert = dp[i][j-1];
+                int del = dp[i-1][j];
+                int replace = dp[i-1][j-1];
+                dp[i][j] = 1 + min3(insert, del, replace);
+
+                // Check for transposition
+                if (i > 1 && j > 1 && s1[i-1] == s2[j-2] && s1[i-2] == s2[j-1]) {
+                    dp[i][j] = min2(dp[i][j], dp[i-2][j-2] + 1);
+                }
             }
         }
     }
-    
-    return d[n][m];
 
+    // Get the result from the table
+    int result = dp[len1][len2];
+
+    // Free allocated memory
+    for (i = 0; i <= len1; i++) {
+        free(dp[i]);
+    }
+    free(dp);
+
+    return result;
 }
 
-/*
 int main() {
-    
-    char* a = "niche";
-    char* b = "chien";
-    
-    int r = distance(a,b);
-    printf("Distance: %d", r);
-    
-
+    printf("%d\n", distance("road", "raod"));
     return 0;
 }
-*/
+
--- levenshtein/levenshtein.h
+++ levenshtein/levenshtein.h
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 
-int min(int a, int b);
-int distance(char* a, char* b);
+
+int min3(int a, int b, int c);
+int min2(int a, int b);
+int distance(const char* a, const char* b);
 
